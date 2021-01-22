bb96b197ac0aa848b3c0df09339dc85b0f1f5756
Tejasvi <soi.tejasvi@gmail.com>
2021-01-22 12:11:28 +0530

    Second Commit made changes to all

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
-$(document).ready(function () {
-
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:1,
+        itemsDesktop:[1000,1],
+        itemsDesktopSmall:[979,1],
+        itemsTablet:[769,1],
+        pagination:true,
+        transitionStyle:"goDown",
+        autoplay:true
+    });
 });
