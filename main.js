d8f55e9852263b75de2891c91eaf776bdc52102d
Tejasvi <soi.tejasvi@gmail.com>
2021-01-22 18:25:19 +0530

    Aligned elemnets

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
+
+ 
 $(document).ready(function(){
     $("#testimonial-slider").owlCarousel({
         items:1,
         itemsDesktop:[1000,1],
         itemsDesktopSmall:[979,1],
-        itemsTablet:[769,1],
+        itemsTablet:[768,1],
         pagination:true,
-        transitionStyle:"goDown",
-        autoplay:true
+        transitionStyle:"backSlide",
+        autoPlay:true
     });
 });
